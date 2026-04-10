6d081633feeec586b2b52eaa8e32218e69551535
aznative77 <aznative77@gmail.com>
2026-04-10 14:26:59 -0700

    Added the clients page and functionality

---
+++ SSSClientWeb/Pages/Clients/Delete.cshtml.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SSSClientWeb.Models;
+
+namespace SSSClientWeb.Pages.Clients
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SssclientContext _context;
+
+        public DeleteModel(SssclientContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Client Client { get; set; } = new Client();
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            var client = await _context.Clients.FindAsync(id);
+            if (client == null)
+                return NotFound();
+
+            Client = client;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var client = await _context.Clients.FindAsync(Client.ClientRec);
+            if (client != null)
+            {
+                _context.Clients.Remove(client);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("Index");
+        }
+    }
+}
