f1c5829867e32943a1db524986cc5e3c9132cd76
aznative77 <aznative77@gmail.com>
2026-06-11 10:42:58 -0700

    Change Ticket Status while Entering Note
    Added the ability to change the ticket status while entering a note.

--- SSSClientTickets/Pages/TicketTime/Create.cshtml.cs
+++ SSSClientTickets/Pages/TicketTime/Create.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SSSClientTickets.Models;
 
@@ -17,22 +18,23 @@ namespace SSSClientTickets.Pages.TicketTime
         [BindProperty]
         public Models.TicketTime TicketEntry { get; set; } = new Models.TicketTime();
 
+        [BindProperty]
+        public int TicketStatusRec { get; set; }
+
         public Ticket? TicketInfo { get; set; }
+        public SelectList StatusList { get; set; } = default!;
 
         public async Task<IActionResult> OnGetAsync(int ticketRec)
         {
-            TicketInfo = await _context.Tickets
-                .Include(t => t.ClientRecNavigation)
-                .Include(t => t.CustomerRecNavigation)
-                .FirstOrDefaultAsync(t => t.TicketRec == ticketRec);
-
-            if (TicketInfo == null)
+            if (!await LoadTicketInfoAsync(ticketRec))
                 return NotFound();
 
             // Pre-populate TicketRec and default times
             TicketEntry.TicketRec = ticketRec;
             TicketEntry.StartTime = DateTime.Now;
             TicketEntry.EndTime = DateTime.Now;
+            TicketStatusRec = TicketInfo!.StatusRec;
+            await PopulateDropdownsAsync();
 
             return Page();
         }
@@ -43,17 +45,37 @@ namespace SSSClientTickets.Pages.TicketTime
 
             if (!ModelState.IsValid)
             {
-                TicketInfo = await _context.Tickets
-                    .Include(t => t.ClientRecNavigation)
-                    .Include(t => t.CustomerRecNavigation)
-                    .FirstOrDefaultAsync(t => t.TicketRec == TicketEntry.TicketRec);
+                await LoadTicketInfoAsync(TicketEntry.TicketRec);
+                await PopulateDropdownsAsync();
                 return Page();
             }
 
+            var ticket = await _context.Tickets.FindAsync(TicketEntry.TicketRec);
+            if (ticket == null)
+                return NotFound();
+
+            ticket.StatusRec = TicketStatusRec;
             _context.TicketTimes.Add(TicketEntry);
             await _context.SaveChangesAsync();
 
             return RedirectToPage("/Tickets/Detail", new { id = TicketEntry.TicketRec });
+        }
+
+        private async Task<bool> LoadTicketInfoAsync(int ticketRec)
+        {
+            TicketInfo = await _context.Tickets
+                .Include(t => t.ClientRecNavigation)
+                .Include(t => t.CustomerRecNavigation)
+                .FirstOrDefaultAsync(t => t.TicketRec == ticketRec);
+
+            return TicketInfo != null;
+        }
+
+        private async Task PopulateDropdownsAsync()
+        {
+            StatusList = new SelectList(
+                await _context.TicketStatuses.ToListAsync(),
+                "StatusRec", "Status", TicketStatusRec);
         }
     }
 }
--- SSSClientTickets/Pages/TicketTime/Edit.cshtml.cs
+++ SSSClientTickets/Pages/TicketTime/Edit.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SSSClientTickets.Models;
 
@@ -17,7 +18,11 @@ namespace SSSClientTickets.Pages.TicketTime
         [BindProperty]
         public Models.TicketTime TicketEntry { get; set; } = new Models.TicketTime();
 
+        [BindProperty]
+        public int TicketStatusRec { get; set; }
+
         public Ticket? TicketInfo { get; set; }
+        public SelectList StatusList { get; set; } = default!;
 
         public async Task<IActionResult> OnGetAsync(int id)
         {
@@ -27,10 +32,11 @@ namespace SSSClientTickets.Pages.TicketTime
 
             TicketEntry = entry;
 
-            TicketInfo = await _context.Tickets
-                .Include(t => t.ClientRecNavigation)
-                .Include(t => t.CustomerRecNavigation)
-                .FirstOrDefaultAsync(t => t.TicketRec == TicketEntry.TicketRec);
+            if (!await LoadTicketInfoAsync(TicketEntry.TicketRec))
+                return NotFound();
+
+            TicketStatusRec = TicketInfo!.StatusRec;
+            await PopulateDropdownsAsync();
 
             return Page();
         }
@@ -41,16 +47,20 @@ namespace SSSClientTickets.Pages.TicketTime
 
             if (!ModelState.IsValid)
             {
-                TicketInfo = await _context.Tickets
-                    .Include(t => t.ClientRecNavigation)
-                    .Include(t => t.CustomerRecNavigation)
-                    .FirstOrDefaultAsync(t => t.TicketRec == TicketEntry.TicketRec);
+                await LoadTicketInfoAsync(TicketEntry.TicketRec);
+                await PopulateDropdownsAsync();
                 return Page();
             }
 
             var entryToUpdate = await _context.TicketTimes.FindAsync(TicketEntry.TimeRec);
             if (entryToUpdate == null)
                 return NotFound();
+
+            var ticket = await _context.Tickets.FindAsync(TicketEntry.TicketRec);
+            if (ticket == null)
+                return NotFound();
+
+            ticket.StatusRec = TicketStatusRec;
 
             await TryUpdateModelAsync(
                 entryToUpdate,
@@ -72,6 +82,23 @@ namespace SSSClientTickets.Pages.TicketTime
             }
 
             return RedirectToPage("/Tickets/Detail", new { id = TicketEntry.TicketRec });
+        }
+
+        private async Task<bool> LoadTicketInfoAsync(int ticketRec)
+        {
+            TicketInfo = await _context.Tickets
+                .Include(t => t.ClientRecNavigation)
+                .Include(t => t.CustomerRecNavigation)
+                .FirstOrDefaultAsync(t => t.TicketRec == ticketRec);
+
+            return TicketInfo != null;
+        }
+
+        private async Task PopulateDropdownsAsync()
+        {
+            StatusList = new SelectList(
+                await _context.TicketStatuses.ToListAsync(),
+                "StatusRec", "Status", TicketStatusRec);
         }
     }
 }
